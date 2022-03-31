31ea65fc25b266caea93067b344414b655e6b218
Microlith57 <microlith57@gmail.com>
2022-03-31 13:32:20 +1300

    initial commit

---
+++ src/TesseractModule.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Celeste.Mod.Tesseract {
+    public class TesseractModule : EverestModule {
+        public static TesseractModule Instance;
+
+        public TesseractModule() {
+            Instance = this;
+        }
+
+        public override void Load() {
+        }
+
+        public override void Unload() {
+    }
+}
